67b567c20613b0358ab60ddf7f0cf9a4bd1fe2f1
Github Action <github-action@mrns.nl>
2026-08-23 02:20:21 +0000

    Update GitHub Action

--- run.sh
+++ run.sh
@@ -4,16 +4,16 @@ mkdir -p names
 
 [ -f "$CACHE/osmosis-0.49.2.zip" ] || wcurl -o $CACHE/osmosis-0.49.2.zip https://github.com/openstreetmap/osmosis/releases/download/0.49.2/osmosis-0.49.2.zip
 echo osmosis-0.49.2.zip > names/osmosis
-[ -f "$CACHE/belgium-260820.osm.pbf" ] || wcurl -o $CACHE/belgium-260820.osm.pbf https://download.geofabrik.de/europe/belgium-260820.osm.pbf
-echo belgium-260820.osm.pbf > names/geofabrik-belgium
-[ -f "$CACHE/netherlands-260820.osm.pbf" ] || wcurl -o $CACHE/netherlands-260820.osm.pbf https://download.geofabrik.de/europe/netherlands-260820.osm.pbf
-echo netherlands-260820.osm.pbf > names/geofabrik-netherlands
-[ -f "$CACHE/luxembourg-260820.osm.pbf" ] || wcurl -o $CACHE/luxembourg-260820.osm.pbf https://download.geofabrik.de/europe/luxembourg-260820.osm.pbf
-echo luxembourg-260820.osm.pbf > names/geofabrik-luxembourg
+[ -f "$CACHE/belgium-260822.osm.pbf" ] || wcurl -o $CACHE/belgium-260822.osm.pbf https://download.geofabrik.de/europe/belgium-260822.osm.pbf
+echo belgium-260822.osm.pbf > names/geofabrik-belgium
+[ -f "$CACHE/netherlands-260822.osm.pbf" ] || wcurl -o $CACHE/netherlands-260822.osm.pbf https://download.geofabrik.de/europe/netherlands-260822.osm.pbf
+echo netherlands-260822.osm.pbf > names/geofabrik-netherlands
+[ -f "$CACHE/luxembourg-260822.osm.pbf" ] || wcurl -o $CACHE/luxembourg-260822.osm.pbf https://download.geofabrik.de/europe/luxembourg-260822.osm.pbf
+echo luxembourg-260822.osm.pbf > names/geofabrik-luxembourg
 [ -f "$CACHE/bounds-20260821.zip" ] || wcurl -o $CACHE/bounds-20260821.zip http://osm.thkukuk.de/data/bounds-20260821.zip
 echo bounds-20260821.zip > names/bounds
-[ -f "$CACHE/sea-20260820220000.zip" ] || wcurl -o $CACHE/sea-20260820220000.zip http://osm.thkukuk.de/data/sea-20260820220000.zip
-echo sea-20260820220000.zip > names/sea
+[ -f "$CACHE/sea-20260821220000.zip" ] || wcurl -o $CACHE/sea-20260821220000.zip http://osm.thkukuk.de/data/sea-20260821220000.zip
+echo sea-20260821220000.zip > names/sea
 [ -f "$CACHE/mkgmap-r4924.zip" ] || wcurl -o $CACHE/mkgmap-r4924.zip https://www.mkgmap.org.uk/download/mkgmap-r4924.zip
 echo mkgmap-r4924.zip > names/mkgmap
 [ -f "$CACHE/splitter-r654.zip" ] || wcurl -o $CACHE/splitter-r654.zip https://www.mkgmap.org.uk/download/splitter-r654.zip
